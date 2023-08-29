ecf68e036be01732123616b7f34215fa48a0165a
TOConde <tomy_t.c@hotmail.com>
2023-08-29 16:23:48 -0300

    implemnto cargar y mostrar inventario

--- ejercicioUno/stockKiosco.ts
+++ ejercicioUno/stockKiosco.ts
@@ -1,20 +1,22 @@
 interface Inventario {
-  mostrarInventario(): void;
-  mostrarVentas(): void;
-  cargarProducto(): void;
+  cargarProducto(producto: Producto): void;
   eliminarProducto(): void;
+  mostrarInventario(): void;
   venderProducto(): void;
+  mostrarVentas(): void;  
 }
 
 class Producto {
   private nombre: string;
   private precio: number;
   private stock: number;
+  private codigo: string;
 
-  constructor(nombre: string, precio: number, stock: number) {
+  constructor(nombre: string, precio: number, stock: number, codigo: string) {
     this.nombre = nombre;
     this.precio = precio;
-    this.stock = stock
+    this.stock = stock;
+    this.codigo = codigo;
   }
 
   getNombre() {
@@ -28,30 +30,40 @@ class Producto {
   getStock() {
     return this.stock
   }
+
+  getCodigo() {
+    return this.codigo
+  }
 }
 
 class Kiosco implements Inventario {
   private productosDisponibles: Producto[] = [];
   private productosVendidos: {producto: Producto; cantidad: number}[] = [];
 
-  venderProducto(): void {
+  cargarProducto(producto: Producto): void {
+    this.productosDisponibles.push(producto);
+  }
+
+  eliminarProducto(): void {
     
   }
 
   mostrarInventario(): void {
+    console.log("*********");
+    console.log("Tenemos en stock:");
+    console.log("");
+    this.productosDisponibles.forEach(producto =>
+      console.log(`Codigo: ${producto.getCodigo}, Producto: ${producto.getNombre}, Precio: ${producto.getPrecio}, Cantidad: ${producto.getStock}`)      
+    );
+    console.log("");
+  }
+
+  venderProducto(): void {
     
   }
 
   mostrarVentas(): void {
     
   }
-  
-  cargarProducto(): void {
-    
-  }
 
-  eliminarProducto(): void {
-    
-  }
-  
 }
